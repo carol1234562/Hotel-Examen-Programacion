7881f3a63ca09047ad66399a77c68950004147ea
Carolina <csolorzanoomar1419@gmail.com>
2026-04-29 11:57:05 +0200

    Creacion de carpeta Exception basico y con un extends de Exception

---
+++ src/main/java/exception/DaoException.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exception;
+
+/**
+ *
+ * @author hanks
+ */
+public class DaoException extends Exception{
+
+    public DaoException() {
+    }
+
+    public DaoException(String message) {
+        super(message);
+    }
+    
+    
+}
